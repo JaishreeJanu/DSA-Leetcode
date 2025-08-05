790c751f1716ed76e406342798833e1ac47432f4
Jaishree Janu <janujaishree94@gmail.com>
2025-08-05 12:27:44 +0200

    Time: 587 ms (52.07%) | Memory: 20.8 MB (36.33%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,17 +3,22 @@ class Solution:
         n = len(nums)
         nums = sorted(nums)
         threesum_triplets = []
-        triplets_hash = {}
 
         for i in range(0,n-2):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             j = i+1
             k = n-1
             while j<k:
                 curr_sum = nums[i]+nums[j]+nums[k]
-                if curr_sum == 0:
-                    if (nums[i],nums[j],nums[k]) not in triplets_hash:
-                        triplets_hash[(nums[i],nums[j],nums[k])] = 1
-                        threesum_triplets.append([nums[i],nums[j],nums[k]])
+                if curr_sum == 0:    
+                    threesum_triplets.append([nums[i],nums[j],nums[k]])
+
+                    while j<k and nums[j]==nums[j+1]:
+                        j += 1
+                    while j<k and nums[k]==nums[k-1]:
+                        k -= 1
+
                     j += 1
                     k -= 1
                 elif curr_sum<0:

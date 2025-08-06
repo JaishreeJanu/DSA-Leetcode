25f8e1ec2e4505362c91c983ca3f654778562e4c
Jaishree Janu <janujaishree94@gmail.com>
2025-08-06 19:08:22 +0200

    Time: 7 ms (93.49%) | Memory: 17.8 MB (82.62%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -3,4 +3,4 @@ class Solution:
         from collections import Counter
         r,m = Counter(ransomNote), Counter(magazine)
 
-        return all(m[c]>=r[c] for c in ransomNote)
+        return all(m[c]>=r[c] for c in r)
